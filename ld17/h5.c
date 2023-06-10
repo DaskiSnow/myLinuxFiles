b2d4a72b019e6084c0a21e56af59aa969cdadb64
daskisnow from Ubuntu-16.04 <437207609@qq.com>
2023-06-10 15:12:17 +0800

    Linux17纠正

--- ld17/h5.c
+++ ld17/h5.c
@@ -4,15 +4,20 @@ int turn = 0; // 实现轮流
 void* threadFun(void* arg) {
     pthread_mutex_t *pmutex = (pthread_mutex_t*)arg;
     while(1){
+        pthread_mutex_lock(pmutex);
         if(turn == 1){
-            pthread_mutex_lock(pmutex);
+            pthread_mutex_unlock(pmutex);
             printf("Before B!\n");
             sleep(3);
             printf("After B\n");
-            pthread_mutex_unlock(pmutex);
             turn = 0;
+            break;
+        }
+        else{
+            pthread_mutex_unlock(pmutex);
         }
     }
+    return NULL;
 }
 
 int main(int argc, char* argv[])
@@ -27,15 +32,21 @@ int main(int argc, char* argv[])
     THREAD_ERROR_CHECK(ret_create, "pthread_create");
 
     while(1){
+        pthread_mutex_lock(&mutex);
         if(turn == 0) {
-            pthread_mutex_lock(&mutex);
+            pthread_mutex_unlock(&mutex);
             printf("Before A!\n");
             sleep(3);
             printf("After A\n");
             turn = 1;
+            break;
+        }
+        else{
             pthread_mutex_unlock(&mutex);
         }
     }
+
+    pthread_join(tid, NULL);
     return 0;
 }
 
