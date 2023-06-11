73d47bec8a830646e335d0d19400b92a039fad85
daskisnow from Ubuntu-16.04 <437207609@qq.com>
2023-06-11 15:10:28 +0800

    纠正了h5

--- ld17/h5_new.c
+++ ld17/h5_new.c
@@ -3,20 +3,22 @@
 int turn = 0; // 实现轮流
 void* threadFun(void* arg) {
     pthread_mutex_t *pmutex = (pthread_mutex_t*)arg;
-    while(1){
+    while(1){ // 轮询flag条件是否成立
         pthread_mutex_lock(pmutex);
-        if(turn == 1){
+        if(turn != 1) { // flag不成立
             pthread_mutex_unlock(pmutex);
-            printf("Before B!\n");
-            sleep(3);
-            printf("After B\n");
-            turn = 0;
-            break;
-        }
-        else{
-            pthread_mutex_unlock(pmutex);
-        }
+            continue;
+        } // flag成立
+        break;
     }
+    // 临界区
+    printf("Before B!\n");
+    sleep(3);
+    printf("After B\n");
+    turn = 0;
+    pthread_mutex_unlock(pmutex);
+    // 临界区end
+
     return NULL;
 }
 
@@ -31,21 +33,22 @@ int main(int argc, char* argv[])
     int ret_create = pthread_create(&tid, NULL, threadFun, &mutex);
     THREAD_ERROR_CHECK(ret_create, "pthread_create");
 
-    while(1){
+    while(1){ // 轮询
         pthread_mutex_lock(&mutex);
-        if(turn == 0) {
+        if(turn != 0) { // 不满足flag
             pthread_mutex_unlock(&mutex);
-            printf("Before A!\n");
-            sleep(3);
-            printf("After A\n");
-            turn = 1;
-            break;
-        }
-        else{
-            pthread_mutex_unlock(&mutex);
-        }
+            continue;
+        } // 满足flag
+        break;
     }
-
+    // 临界区
+    printf("Before A!\n");
+    sleep(3);
+    printf("After A\n");
+    turn = 1;
+    pthread_mutex_unlock(&mutex);
+    // 临界区end
+    
     pthread_join(tid, NULL);
     return 0;
 }
