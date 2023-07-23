53f2445a1803af8ae8bb99a45c01da9b6f577d1e
daskisnow from Ubuntu-16.04 <437207609@qq.com>
2023-07-23 21:05:41 +0800

    cppd22

---
+++ cppd22/a.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std:: ifstream;
+
+
+
+int main(int argc, char* argv[])
+{
+}
+
