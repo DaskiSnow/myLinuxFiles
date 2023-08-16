553bc896b460a53320dc5b25bf0f0c32e4a43019
daskisnow <437207609@qq.com>
2023-08-16 20:40:38 +0800

    CMake的学习

---
+++ CMakeDemo/build/src/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../../bin/main"
+  "../../bin/main.pdb"
+  "CMakeFiles/main.dir/func.cc.o"
+  "CMakeFiles/main.dir/func.cc.o.d"
+  "CMakeFiles/main.dir/main.cc.o"
+  "CMakeFiles/main.dir/main.cc.o.d"
+  "CMakeFiles/main.dir/testFunc1.cc.o"
+  "CMakeFiles/main.dir/testFunc1.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
